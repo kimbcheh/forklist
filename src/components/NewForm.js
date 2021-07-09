f95ce50bdccfc1829d3ac5f54a0dfaf732fc790f
kimbcheh <kimbcheh@gmail.com>
2021-07-09 14:52:35 +1000

    add basic form elements

--- src/components/NewForm.js
+++ src/components/NewForm.js
@@ -1,8 +1,28 @@
+import { Button, Form, Input, Radio } from 'antd'
+
 function NewForm() {
  return (
-  <div>
-   <p>NewForm</p>
-  </div>
+  <Form name='new'>
+   <Form.Item label='Restaurant Name' name='restaurant'>
+    <Input />
+   </Form.Item>
+   <Form.Item label='Suburb' name='suburb'>
+    <Input />
+   </Form.Item>
+   <Form.Item label='Price range' name='price'>
+    <Radio.Group>
+     <Radio value={1}>$</Radio>
+     <Radio value={2}>$$</Radio>
+     <Radio value={3}>$$$</Radio>
+    </Radio.Group>
+   </Form.Item>
+   <Form.Item label='Note (optional)' name='note'>
+    <Input />
+   </Form.Item>
+   <Form.Item>
+    <Button type='primary'>Submit</Button>
+   </Form.Item>
+  </Form>
  )
 }
 
