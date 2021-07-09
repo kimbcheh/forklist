97cdd0a537b6d496414cfaff9a703c694debb8b5
Kimberly <83323970+kimbcheh@users.noreply.github.com>
2021-07-09 14:27:38 +1000

    feat: add basic layout components (#2)
    * add layout component and set up columns
    
    * add h1 heading
    
    * add responsive breakpoints to wrap columns

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import './App.less'
+import { Col, Layout, Row } from 'antd'
+
+const { Header, Content, Footer } = Layout
 
 function App() {
  return (
-  <div>
-   <h1>Forklist</h1>
-  </div>
+  <Layout>
+   <Header style={{ backgroundColor: '#fff' }}>
+    <h1>Forklist</h1>
+   </Header>
+   <Content style={{ backgroundColor: '#fff', padding: '0 50px' }}>
+    <Row>
+     <Col xs={24} md={12} style={{ backgroundColor: 'pink' }}>
+      Content Left (form to add restaurant)
+     </Col>
+     <Col xs={24} md={12} style={{ backgroundColor: 'orange' }}>
+      Content Right (list of restaurant cards)
+     </Col>
+    </Row>
+   </Content>
+   <Footer style={{ backgroundColor: '#fff' }}>Made by Kimberly Cheh</Footer>
+  </Layout>
  )
 }
 
