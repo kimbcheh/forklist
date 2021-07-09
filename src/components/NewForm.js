9bed5b6a2e9d6c1db21113258dbda05904bb5ad3
kimbcheh <kimbcheh@gmail.com>
2021-07-09 16:31:05 +1000

    change form layout type to vertical

--- src/components/NewForm.js
+++ src/components/NewForm.js
@@ -1,68 +1,62 @@
-import { Button, Form, Input, Radio } from 'antd'
+import { Button, Card, Form, Input, Radio } from 'antd'
 
 const { TextArea } = Input
 
 function NewForm() {
  return (
-  <Form
-   name='new'
-   labelCol={{
-    span: 4,
-   }}
-   wrapperCol={{
-    span: 16,
-   }}
-  >
-   <Form.Item
-    label='Restaurant'
-    name='restaurant'
-    rules={[
-     { required: true, message: 'Please input the name of the restaurant!' },
-    ]}
-   >
-    <Input placeholder='e.g. Napier Quarter' />
-   </Form.Item>
-   <Form.Item
-    label='Suburb'
-    name='suburb'
-    rules={[
-     {
-      required: true,
-      message: 'Please input the suburb where the restaurant is located!',
-     },
-    ]}
-   >
-    <Input placeholder='e.g. Fitzroy' />
-   </Form.Item>
-   <Form.Item
-    label='Price'
-    name='price'
-    rules={[
-     {
-      required: true,
-      message: 'Please select the price range of the restaurant!',
-     },
-    ]}
-   >
-    <Radio.Group>
-     <Radio value={1}>$</Radio>
-     <Radio value={2}>$$</Radio>
-     <Radio value={3}>$$$</Radio>
-    </Radio.Group>
-   </Form.Item>
-   <Form.Item label='Note (optional)' name='note'>
-    <TextArea
-     showCount
-     maxLength={100}
-     placeholder='e.g. Michelle recommended the chocolate and sea salt brownie'
-    />
-   </Form.Item>
-   <Form.Item wrapperCol={{ span: 14, offset: 4 }}>
-    <Button type='primary' htmlType='submit'>
-     Submit
-    </Button>
-   </Form.Item>
-  </Form>
+  <Card>
+   <Form name='new' layout='vertical'>
+    <Form.Item
+     label='Restaurant'
+     name='restaurant'
+     rules={[
+      { required: true, message: 'Please input the name of the restaurant!' },
+     ]}
+    >
+     <Input placeholder='e.g. Napier Quarter' />
+    </Form.Item>
+    <Form.Item
+     label='Suburb'
+     name='suburb'
+     rules={[
+      {
+       required: true,
+       message: 'Please input the suburb where the restaurant is located!',
+      },
+     ]}
+    >
+     <Input placeholder='e.g. Fitzroy' />
+    </Form.Item>
+    <Form.Item
+     label='Price'
+     name='price'
+     rules={[
+      {
+       required: true,
+       message: 'Please select the price range of the restaurant!',
+      },
+     ]}
+    >
+     <Radio.Group>
+      <Radio value={1}>$</Radio>
+      <Radio value={2}>$$</Radio>
+      <Radio value={3}>$$$</Radio>
+     </Radio.Group>
+    </Form.Item>
+    <Form.Item label='Notes' name='notes'>
+     <TextArea
+      showCount
+      maxLength={100}
+      placeholder='e.g. Michelle recommended the chocolate and sea salt brownie'
+     />
+    </Form.Item>
+    <Form.Item>
+     <Button type='primary' htmlType='submit'>
+      Submit
+     </Button>
+    </Form.Item>
+   </Form>
+  </Card>
  )
 }
 
