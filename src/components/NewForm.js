9f5624ea87e55f1f241839840b7e8cd4dbadc3fd
kimbcheh <kimbcheh@gmail.com>
2021-07-09 15:06:57 +1000

    add required rule to certain form elements

--- src/components/NewForm.js
+++ src/components/NewForm.js
@@ -3,13 +3,37 @@ import { Button, Form, Input, Radio } from 'antd'
 function NewForm() {
  return (
   <Form name='new'>
-   <Form.Item label='Restaurant Name' name='restaurant'>
+   <Form.Item
+    label='Restaurant Name'
+    name='restaurant'
+    rules={[
+     { required: true, message: 'Please input the name of the restaurant!' },
+    ]}
+   >
     <Input />
    </Form.Item>
-   <Form.Item label='Suburb' name='suburb'>
+   <Form.Item
+    label='Suburb'
+    name='suburb'
+    rules={[
+     {
+      required: true,
+      message: 'Please input the suburb where the restaurant is located!',
+     },
+    ]}
+   >
     <Input />
    </Form.Item>
-   <Form.Item label='Price range' name='price'>
+   <Form.Item
+    label='Price range'
+    name='price'
+    rules={[
+     {
+      required: true,
+      message: 'Please select the price range of the restaurant!',
+     },
+    ]}
+   >
     <Radio.Group>
      <Radio value={1}>$</Radio>
      <Radio value={2}>$$</Radio>
@@ -20,7 +44,9 @@ function NewForm() {
     <Input />
    </Form.Item>
    <Form.Item>
-    <Button type='primary'>Submit</Button>
+    <Button type='primary' htmlType='submit'>
+     Submit
+    </Button>
    </Form.Item>
   </Form>
  )
