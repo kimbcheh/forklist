1487f6f967bf3126b2bf517af0ed06c98112aa5a
kimbcheh <kimbcheh@gmail.com>
2021-07-09 14:12:14 +1000

    add layout component and set up columns

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import './App.less'
+import { Col, Layout, Row } from 'antd'
+
+const { Header, Content, Footer } = Layout
 
 function App() {
  return (
-  <div>
-   <h1>Forklist</h1>
-  </div>
+  <Layout>
+   <Header style={{ backgroundColor: '#fff' }}>Forklist</Header>
+   <Content style={{ backgroundColor: '#fff', padding: '0 50px' }}>
+    <Row>
+     <Col span={12}>Content Left</Col>
+     <Col span={12}>Content Right</Col>
+    </Row>
+   </Content>
+   <Footer style={{ backgroundColor: '#fff' }}>Made by Kimberly Cheh</Footer>
+  </Layout>
  )
 }
 
