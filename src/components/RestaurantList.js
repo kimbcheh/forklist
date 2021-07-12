020d8b20dae6220c06081de58d83f507c64444e1
kimbcheh <kimbcheh@gmail.com>
2021-07-12 14:12:09 +1000

    add basic card component

--- src/components/RestaurantList.js
+++ src/components/RestaurantList.js
@@ -1,7 +1,12 @@
+import { Card } from 'antd'
+
 function RestaurantList() {
  return (
   <div>
    <p>RestaurantList component</p>
+   <Card style={{ width: '400px' }} title='Napier Quarter'>
+    Michelle recommended the chocolate and sea salt brownie.
+   </Card>
   </div>
  )
 }
