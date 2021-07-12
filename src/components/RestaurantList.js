6d93c8367d38b974a5c1fc847d5294372f549f6d
kimbcheh <kimbcheh@gmail.com>
2021-07-12 15:05:06 +1000

    add vertical spacing to card list

--- src/components/RestaurantList.js
+++ src/components/RestaurantList.js
@@ -1,30 +1,32 @@
-import { Card, Tag } from 'antd'
+import { Card, Space, Tag } from 'antd'
 import { EditOutlined, DeleteOutlined } from '@ant-design/icons'
 
 function RestaurantList() {
  return (
   <Card>
-   <h2>Restaurant List</h2>
-   <Card
-    title='Napier Quarter'
-    actions={[<EditOutlined />, <DeleteOutlined />]}
-   >
-    <p>Michelle recommended the chocolate and sea salt brownie</p>
-    <div>
-     <Tag color='purple'>Fitzroy</Tag> <Tag color='green'>$$</Tag>
-    </div>
-   </Card>
-   <Card title='Aru' actions={[<EditOutlined />, <DeleteOutlined />]}>
-    <p>Khanh Nguyen's new restaurant</p>
-    <div>
-     <Tag color='purple'>CBD</Tag> <Tag color='green'>$$$</Tag>
-    </div>
-   </Card>
-   <Card title='Marko' actions={[<EditOutlined />, <DeleteOutlined />]}>
-    <div>
-     <Tag color='purple'>South Melbourne</Tag> <Tag color='green'>$</Tag>
-    </div>
-   </Card>
+   <Space direction='vertical' style={{ width: '100%' }}>
+    <h2>Restaurant List</h2>
+    <Card
+     title='Napier Quarter'
+     actions={[<EditOutlined />, <DeleteOutlined />]}
+    >
+     <p>Michelle recommended the chocolate and sea salt brownie</p>
+     <div>
+      <Tag color='purple'>Fitzroy</Tag> <Tag color='green'>$$</Tag>
+     </div>
+    </Card>
+    <Card title='Aru' actions={[<EditOutlined />, <DeleteOutlined />]}>
+     <p>Khanh Nguyen's new restaurant</p>
+     <div>
+      <Tag color='purple'>CBD</Tag> <Tag color='green'>$$$</Tag>
+     </div>
+    </Card>
+    <Card title='Marko' actions={[<EditOutlined />, <DeleteOutlined />]}>
+     <div>
+      <Tag color='purple'>South Melbourne</Tag> <Tag color='green'>$</Tag>
+     </div>
+    </Card>
+   </Space>
   </Card>
  )
 }
