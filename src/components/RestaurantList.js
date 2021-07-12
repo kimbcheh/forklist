b165cf402e50908265c37bf928178f2733effde5
kimbcheh <kimbcheh@gmail.com>
2021-07-12 14:25:29 +1000

    add icons to card for edit and delete

--- src/components/RestaurantList.js
+++ src/components/RestaurantList.js
@@ -1,10 +1,14 @@
 import { Card, Tag } from 'antd'
+import { EditOutlined, DeleteOutlined } from '@ant-design/icons'
 
 function RestaurantList() {
  return (
   <div>
    <p>RestaurantList component</p>
-   <Card title='Napier Quarter' actions={[<p>Edit</p>, <p>Remove</p>]}>
+   <Card
+    title='Napier Quarter'
+    actions={[<EditOutlined />, <DeleteOutlined />]}
+   >
     <p>Michelle recommended the chocolate and sea salt brownie</p>
     <div>
      <Tag color='purple'>Fitzroy</Tag> <Tag color='green'>$$</Tag>
