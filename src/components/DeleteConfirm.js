d88748547922ceeb0244f24777ba463384ae46c9
kimbcheh <kimbcheh@gmail.com>
2021-07-19 14:05:12 +1000

    add delete functionality to modal

--- src/components/DeleteConfirm.js
+++ src/components/DeleteConfirm.js
@@ -1,13 +1,30 @@
+import { Modal } from 'antd'
+
 function DeleteConfirm({
  modalItem,
  isDeleteModalVisible,
  setIsDeleteModalVisible,
  deleteItem,
 }) {
+ const handleDeleteOk = () => {
+  deleteItem(modalItem.id)
+  setIsDeleteModalVisible(false)
+ }
+
+ const handleDeleteCancel = () => {
+  setIsDeleteModalVisible(false)
+ }
+
  return (
-  <div>
-   <p>DeleteConfirm</p>
-  </div>
+  <Modal
+   title={`Delete: ${modalItem.restaurant}`}
+   visible={isDeleteModalVisible}
+   onOk={handleDeleteOk}
+   onCancel={handleDeleteCancel}
+   okText='Confirm'
+  >
+   <p>Are you sure you want to delete {modalItem.restaurant} from your list?</p>
+  </Modal>
  )
 }
 
