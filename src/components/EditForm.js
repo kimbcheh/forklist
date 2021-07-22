ddf3bd326f219a019fb4c853a17296f09f4898b6
Kimberly <83323970+kimbcheh@users.noreply.github.com>
2021-07-22 15:11:42 +1000

    refactor: move form content into separate component (#11)
    * move form from newform into new component
    
    * add conditional rendering for submit button
    
    * add form name prop
    
    * add form content to edit form modal
    
    * remove conditional rendering of submit button

--- src/components/EditForm.js
+++ src/components/EditForm.js
@@ -1,7 +1,5 @@
-import { useEffect, useRef } from 'react'
-import { Form, Input, Modal, Radio } from 'antd'
-
-const { TextArea } = Input
+import FormContent from './FormContent'
+import { Modal } from 'antd'
 
 function EditForm({
  isEditModalVisible,
@@ -9,22 +7,9 @@ function EditForm({
  modalItem,
  editItem,
 }) {
- const [form] = Form.useForm()
-
- const firstRender = useRef(true)
-
- useEffect(() => {
-  if (firstRender.current) {
-   firstRender.current = false
-  } else {
-   form.setFieldsValue(modalItem)
-  }
- }, [form, modalItem])
-
- const handleEditOk = () => {
-  form.validateFields().then((values) => {
-   editItem(modalItem.id, values)
-  })
+ function onFinish(values) {
+  console.log(values)
+  editItem(modalItem.id, values)
   setIsEditModalVisible(false)
  }
 
@@ -35,100 +20,16 @@ function EditForm({
   <Modal
    title={`Edit: ${modalItem.restaurant}`}
    visible={isEditModalVisible}
-   onOk={handleEditOk}
    onCancel={handleEditCancel}
+   footer={null}
    destroyOnClose
   >
-   <Form
-    form={form}
-    name='edit'
-    layout='vertical'
-    preserve={false}
+   <FormContent
+    formName='edit'
+    preserveForm={false}
     initialValues={modalItem}
-   >
-    <Form.Item
-     label='Restaurant'
-     name='restaurant'
-     rules={[
-      { required: true, message: 'Please input the name of the restaurant!' },
-      {
-       whitespace: true,
-       message: 'Please input the name of the restaurant!',
-      },
-     ]}
-    >
-     <Input placeholder='e.g. Napier Quarter' />
-    </Form.Item>
-    <Form.Item
-     label='Suburb'
-     name='suburb'
-     rules={[
-      {
-       required: true,
-       message: 'Please input the suburb where the restaurant is located!',
-      },
-      {
-       whitespace: true,
-       message: 'Please input the suburb where the restaurant is located!',
-      },
-     ]}
-    >
-     <Input placeholder='e.g. Fitzroy' />
-    </Form.Item>
-    <Form.Item
-     label='Type'
-     name='type'
-     rules={[
-      {
-       required: true,
-       message: 'Please select the type of restaurant!',
-      },
-     ]}
-    >
-     <Radio.Group>
-      <Radio value={'Restaurant'}>Restaurant</Radio>
-      <Radio value={'Cafe'}>Cafe</Radio>
-      <Radio value={'Bar'}>Bar</Radio>
-      <Radio value={'Dessert'}>Dessert</Radio>
-     </Radio.Group>
-    </Form.Item>
-    <Form.Item
-     label='Price'
-     name='price'
-     rules={[
-      {
-       required: true,
-       message: 'Please select the price range of the restaurant!',
-      },
-     ]}
-    >
-     <Radio.Group>
-      <Radio value={'$'}>$</Radio>
-      <Radio value={'$$'}>$$</Radio>
-      <Radio value={'$$$'}>$$$</Radio>
-     </Radio.Group>
-    </Form.Item>
-    <Form.Item
-     label='Link'
-     name='link'
-     type='url'
-     rules={[
-      {
-       type: 'url',
-       message: 'Please input a valid URL!',
-      },
-     ]}
-    >
-     <Input placeholder='e.g. http://napierquarter.com.au/' />
-    </Form.Item>
-    <Form.Item label='Notes' name='notes'>
-     <TextArea
-      showCount
-      maxLength={100}
-      placeholder='e.g. Michelle recommended the chocolate and sea salt brownie'
-     />
-    </Form.Item>
-   </Form>
+    onFinish={onFinish}
+   />
   </Modal>
  )
 }
---
+++ src/components/FormContent.js
@@ -0,0 +1,115 @@
+import { Button, Form, Input, Radio } from 'antd'
+
+const { TextArea } = Input
+
+function FormContent({ onFinish, formName, preserveForm, initialValues }) {
+ const [form] = Form.useForm()
+
+ function onFormFinish(values) {
+  onFinish(values)
+  form.resetFields()
+ }
+
+ return (
+  <div>
+   <Form
+    name={formName}
+    form={form}
+    layout='vertical'
+    onFinish={onFormFinish}
+    preserve={preserveForm}
+    initialValues={initialValues}
+   >
+    <Form.Item
+     label='Restaurant'
+     name='restaurant'
+     rules={[
+      { required: true, message: 'Please input the name of the restaurant!' },
+      {
+       whitespace: true,
+       message: 'Please input the name of the restaurant!',
+      },
+     ]}
+    >
+     <Input placeholder='e.g. Napier Quarter' />
+    </Form.Item>
+    <Form.Item
+     label='Suburb'
+     name='suburb'
+     rules={[
+      {
+       required: true,
+       message: 'Please input the suburb where the restaurant is located!',
+      },
+      {
+       whitespace: true,
+       message: 'Please input the suburb where the restaurant is located!',
+      },
+     ]}
+    >
+     <Input placeholder='e.g. Fitzroy' />
+    </Form.Item>
+    <Form.Item
+     label='Type'
+     name='type'
+     rules={[
+      {
+       required: true,
+       message: 'Please select the type of restaurant!',
+      },
+     ]}
+    >
+     <Radio.Group>
+      <Radio value={'Restaurant'}>Restaurant</Radio>
+      <Radio value={'Cafe'}>Cafe</Radio>
+      <Radio value={'Bar'}>Bar</Radio>
+      <Radio value={'Dessert'}>Dessert</Radio>
+     </Radio.Group>
+    </Form.Item>
+    <Form.Item
+     label='Price'
+     name='price'
+     rules={[
+      {
+       required: true,
+       message: 'Please select the price range of the restaurant!',
+      },
+     ]}
+    >
+     <Radio.Group>
+      <Radio value={'$'}>$</Radio>
+      <Radio value={'$$'}>$$</Radio>
+      <Radio value={'$$$'}>$$$</Radio>
+     </Radio.Group>
+    </Form.Item>
+    <Form.Item
+     label='Link'
+     name='link'
+     type='url'
+     rules={[
+      {
+       type: 'url',
+       message: 'Please input a valid URL!',
+      },
+     ]}
+    >
+     <Input placeholder='e.g. http://napierquarter.com.au/' />
+    </Form.Item>
+    <Form.Item label='Notes' name='notes'>
+     <TextArea
+      showCount
+      maxLength={100}
+      placeholder='e.g. Michelle recommended the chocolate and sea salt brownie'
+     />
+    </Form.Item>
+    <Form.Item>
+     <Button type='primary' htmlType='submit'>
+      Submit
+     </Button>
+    </Form.Item>
+   </Form>
+  </div>
+ )
+}
+
+export default FormContent
