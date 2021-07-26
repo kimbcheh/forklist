2b77b64486a9bcbd0ee883046d4dd065b1379516
kimbcheh <kimbcheh@gmail.com>
2021-07-26 11:15:18 +1000

    add form element to wrap filters

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,16 +1,29 @@
-import { Select } from 'antd'
+import { Button, Form, Select } from 'antd'
+import { useState } from 'react'
 
 const { Option } = Select
 
 function Filter({ suburbList }) {
+ const [filterCriteria, setFilterCriteria] = useState()
+
+ function onFinish(values) {
+  console.log(values)
+ }
+
  return (
   <div>
-   <p>Filter by suburbs:</p>
-   <Select mode='multiple' allowClear style={{ width: '100%' }}>
-    {suburbList.map((suburb) => {
-     return <Option value={suburb}>{suburb}</Option>
-    })}
-   </Select>
+   <Form name='filter' onFinish={onFinish}>
+    <Form.Item label='Filter by suburb:' name='suburbFilter'>
+     <Select mode='multiple' allowClear style={{ width: '100%' }}>
+      {suburbList.map((suburb) => {
+       return <Option value={suburb}>{suburb}</Option>
+      })}
+     </Select>
+    </Form.Item>
+    <Form.Item>
+     <Button htmlType='submit'>Filter</Button>
+    </Form.Item>
+   </Form>
   </div>
  )
 }
