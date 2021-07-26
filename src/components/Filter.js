80632e248afc21c0d78563253ab93deab5f804d1
kimbcheh <kimbcheh@gmail.com>
2021-07-26 11:01:30 +1000

    add multiple select component for suburb list in filter

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,7 +1,16 @@
-function Filter(startData) {
+import { Select } from 'antd'
+
+const { Option } = Select
+
+function Filter({ suburbList }) {
  return (
   <div>
-   <p>Filter Component</p>
+   <p>Filter by suburbs:</p>
+   <Select mode='multiple' allowClear style={{ width: '100%' }}>
+    {suburbList.map((suburb) => {
+     return <Option value={suburb}>{suburb}</Option>
+    })}
+   </Select>
   </div>
  )
 }
