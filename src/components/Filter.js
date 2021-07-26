b7afa4ff7a9fca4438b55bdf847dd532446794b3
kimbcheh <kimbcheh@gmail.com>
2021-07-26 14:55:28 +1000

    add collapse component for filter form

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,6 +1,7 @@
-import { Button, Form, Radio, Select } from 'antd'
+import { Button, Collapse, Form, Radio, Select } from 'antd'
 
 const { Option } = Select
+const { Panel } = Collapse
 
 function Filter({ suburbList, setFilterCriteria }) {
  const [form] = Form.useForm()
@@ -15,44 +16,48 @@ function Filter({ suburbList, setFilterCriteria }) {
 
  return (
   <div>
-   <Form name='filter' onFinish={onFinish} form={form}>
-    <Form.Item label='Filter by suburb:' name='suburb'>
-     <Select allowClear style={{ width: '100%' }}>
-      {suburbList.map((suburb) => {
-       return (
-        <Option value={suburb} key={suburb}>
-         {suburb}
-        </Option>
-       )
-      })}
-     </Select>
-    </Form.Item>
-    <Form.Item label='Filter by type' name='type'>
-     <Radio.Group>
-      <Radio value={'Restaurant'}>Restaurant</Radio>
-      <Radio value={'Cafe'}>Cafe</Radio>
-      <Radio value={'Bar'}>Bar</Radio>
-      <Radio value={'Dessert'}>Dessert</Radio>
-     </Radio.Group>
-    </Form.Item>
-    <Form.Item label='Filter by price' name='price'>
-     <Radio.Group>
-      <Radio value={'$'}>$</Radio>
-      <Radio value={'$$'}>$$</Radio>
-      <Radio value={'$$$'}>$$$</Radio>
-     </Radio.Group>
-    </Form.Item>
-    <Form.Item>
-     <Button htmlType='submit' type='primary'>
-      Filter
-     </Button>
-    </Form.Item>
-    <Form.Item>
-     <Button htmlType='button' onClick={clearFilter}>
-      Clear filter
-     </Button>
-    </Form.Item>
-   </Form>
+   <Collapse>
+    <Panel header='Select filter options'>
+     <Form name='filter' onFinish={onFinish} form={form} layout='vertical'>
+      <Form.Item label='Filter by suburb:' name='suburb'>
+       <Select allowClear style={{ width: '100%' }}>
+        {suburbList.map((suburb) => {
+         return (
+          <Option value={suburb} key={suburb}>
+           {suburb}
+          </Option>
+         )
+        })}
+       </Select>
+      </Form.Item>
+      <Form.Item label='Filter by type' name='type'>
+       <Radio.Group>
+        <Radio value={'Restaurant'}>Restaurant</Radio>
+        <Radio value={'Cafe'}>Cafe</Radio>
+        <Radio value={'Bar'}>Bar</Radio>
+        <Radio value={'Dessert'}>Dessert</Radio>
+       </Radio.Group>
+      </Form.Item>
+      <Form.Item label='Filter by price' name='price'>
+       <Radio.Group>
+        <Radio value={'$'}>$</Radio>
+        <Radio value={'$$'}>$$</Radio>
+        <Radio value={'$$$'}>$$$</Radio>
+       </Radio.Group>
+      </Form.Item>
+      <Form.Item>
+       <Button htmlType='submit' type='primary'>
+        Filter
+       </Button>
+      </Form.Item>
+      <Form.Item>
+       <Button htmlType='button' onClick={clearFilter}>
+        Clear filter
+       </Button>
+      </Form.Item>
+     </Form>
+    </Panel>
+   </Collapse>
   </div>
  )
 }
