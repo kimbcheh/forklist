08c39e4b27480b8f35732b7e93268815eea0e4d0
kimbcheh <kimbcheh@gmail.com>
2021-07-26 11:57:29 +1000

    add clear filter button

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -3,14 +3,19 @@ import { Button, Form, Select } from 'antd'
 const { Option } = Select
 
 function Filter({ suburbList, setFilterCriteria }) {
+ const [form] = Form.useForm()
  function onFinish(values) {
   console.log(values)
   setFilterCriteria(values)
  }
+ function clearFilter() {
+  setFilterCriteria()
+  form.resetFields()
+ }
 
  return (
   <div>
-   <Form name='filter' onFinish={onFinish}>
+   <Form name='filter' onFinish={onFinish} form={form}>
     <Form.Item label='Filter by suburb:' name='suburb'>
      <Select allowClear style={{ width: '100%' }}>
       {suburbList.map((suburb) => {
@@ -23,7 +28,14 @@ function Filter({ suburbList, setFilterCriteria }) {
      </Select>
     </Form.Item>
     <Form.Item>
-     <Button htmlType='submit'>Filter</Button>
+     <Button htmlType='submit' type='primary'>
+      Filter
+     </Button>
+    </Form.Item>
+    <Form.Item>
+     <Button htmlType='button' onClick={clearFilter}>
+      Clear filter
+     </Button>
     </Form.Item>
    </Form>
   </div>
