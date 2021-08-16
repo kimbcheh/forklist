8a89051f89d980b438a062a8fa98f88ca6ad122b
kimbcheh <kimbcheh@gmail.com>
2021-08-16 10:30:29 +1000

    comment out filter and sort data temporarily

--- src/components/RestaurantList.js
+++ src/components/RestaurantList.js
@@ -7,65 +7,65 @@ import { Card, Empty, Space, Tag } from 'antd'
 import { EditOutlined, DeleteOutlined } from '@ant-design/icons'
 
 function RestaurantList({ data, deleteItem, editItem }) {
- const [isDeleteModalVisible, setIsDeleteModalVisible] = useState(false)
- const [isEditModalVisible, setIsEditModalVisible] = useState(false)
- const [modalItem, setModalItem] = useState(data[0])
- const [suburbList, setSuburbList] = useState(null)
- const [filterCriteria, setFilterCriteria] = useState()
- const [filteredData, setFilteredData] = useState(data)
+ //  const [isDeleteModalVisible, setIsDeleteModalVisible] = useState(false)
+ //  const [isEditModalVisible, setIsEditModalVisible] = useState(false)
+ //  const [modalItem, setModalItem] = useState(data[0])
+ //  const [suburbList, setSuburbList] = useState(null)
+ //  const [filterCriteria, setFilterCriteria] = useState()
+ //  const [filteredData, setFilteredData] = useState(data)
 
- const showDeleteModal = (id) => {
-  let dataItem = data.find((item) => item.id === id)
-  setModalItem(dataItem)
-  setIsDeleteModalVisible(true)
- }
+ //  const showDeleteModal = (id) => {
+ //   let dataItem = data.find((item) => item.id === id)
+ //   setModalItem(dataItem)
+ //   setIsDeleteModalVisible(true)
+ //  }
 
- const showEditModal = (id) => {
-  let dataItem = data.find((item) => item.id === id)
-  setModalItem(dataItem)
-  setIsEditModalVisible(true)
- }
+ //  const showEditModal = (id) => {
+ //   let dataItem = data.find((item) => item.id === id)
+ //   setModalItem(dataItem)
+ //   setIsEditModalVisible(true)
+ //  }
 
- useEffect(() => {
-  const list = data.map((item) => {
-   return item.suburb
-  })
-  const uniqueList = [...new Set(list)]
-  const orderedList = uniqueList.sort()
-  setSuburbList(orderedList)
- }, [data])
+ //  useEffect(() => {
+ //   const list = data.map((item) => {
+ //    return item.suburb
+ //   })
+ //   const uniqueList = [...new Set(list)]
+ //   const orderedList = uniqueList.sort()
+ //   setSuburbList(orderedList)
+ //  }, [data])
 
- useEffect(() => {
-  function bySuburb(obj) {
-   if (!filterCriteria.suburb) {
-    return true
-   } else return obj.suburb === filterCriteria.suburb
-  }
-  function byType(obj) {
-   if (!filterCriteria.type) {
-    return true
-   } else return obj.type === filterCriteria.type
-  }
-  function byPrice(obj) {
-   if (!filterCriteria.price) {
-    return true
-   }
-   return obj.price === filterCriteria.price
-  }
-  if (!filterCriteria) {
-   setFilteredData(data)
-   return
-  } else {
-   let filteredList = data.filter(bySuburb).filter(byType).filter(byPrice)
-   setFilteredData(filteredList)
-  }
- }, [data, filterCriteria])
+ //  useEffect(() => {
+ //   function bySuburb(obj) {
+ //    if (!filterCriteria.suburb) {
+ //     return true
+ //    } else return obj.suburb === filterCriteria.suburb
+ //   }
+ //   function byType(obj) {
+ //    if (!filterCriteria.type) {
+ //     return true
+ //    } else return obj.type === filterCriteria.type
+ //   }
+ //   function byPrice(obj) {
+ //    if (!filterCriteria.price) {
+ //     return true
+ //    }
+ //    return obj.price === filterCriteria.price
+ //   }
+ //   if (!filterCriteria) {
+ //    setFilteredData(data)
+ //    return
+ //   } else {
+ //    let filteredList = data.filter(bySuburb).filter(byType).filter(byPrice)
+ //    setFilteredData(filteredList)
+ //   }
+ //  }, [data, filterCriteria])
 
  return (
   <Card>
    <Space direction='vertical' style={{ width: '100%' }}>
     <h2>Restaurant List</h2>
-    {suburbList && (
+    {/* {suburbList && (
      <Filter suburbList={suburbList} setFilterCriteria={setFilterCriteria} />
     )}
     {filterCriteria && (
@@ -81,15 +81,15 @@ function RestaurantList({ data, deleteItem, editItem }) {
     )}
     {filteredData.length < 1 && (
      <Empty description={<span>Sorry no restaurants found!</span>} />
-    )}
-    {filteredData.map((item) => {
+    )} */}
+    {data.map((item) => {
      return (
       <Card
        key={item.id}
-       actions={[
-        <EditOutlined onClick={() => showEditModal(item.id)} />,
-        <DeleteOutlined onClick={() => showDeleteModal(item.id)} />,
-       ]}
+       //    actions={[
+       //     <EditOutlined onClick={() => showEditModal(item.id)} />,
+       //     <DeleteOutlined onClick={() => showDeleteModal(item.id)} />,
+       //    ]}
       >
        <Space>
         <TypeEmoji type={item.type} />
@@ -116,7 +116,7 @@ function RestaurantList({ data, deleteItem, editItem }) {
      )
     })}
    </Space>
-   <DeleteConfirm
+   {/* <DeleteConfirm
     modalItem={modalItem}
     isDeleteModalVisible={isDeleteModalVisible}
     setIsDeleteModalVisible={setIsDeleteModalVisible}
@@ -127,7 +127,7 @@ function RestaurantList({ data, deleteItem, editItem }) {
     isEditModalVisible={isEditModalVisible}
     setIsEditModalVisible={setIsEditModalVisible}
     editItem={editItem}
-   />
+   /> */}
   </Card>
  )
 }
